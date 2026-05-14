92988e286f1135fbb7b2307b0b8de4bb68a8def6
Bootes <matthewliu58@outlook.com>
2026-05-14 15:04:03 +0800

    del pool function for tunneling

--- data-proxy/tunnel-manager/tunnel-listener.go
+++ data-proxy/tunnel-manager/tunnel-listener.go
@@ -6,7 +6,6 @@ import (
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
-	"data-proxy/config"
 	"encoding/binary"
 	"encoding/pem"
 	"errors"
@@ -20,61 +19,7 @@ import (
 	"github.com/quic-go/quic-go"
 )
 
-const defaultPoolSize = 1024
-const defaultPoolCount = 256
-
-// bufferedPool is a bounded pool of byte buffers
-type bufferedPool struct {
-	pool    chan []byte
-	bufSize int
-}
-
-func newBufferedPool(count, bufSize int) *bufferedPool {
-	p := &bufferedPool{
-		pool:    make(chan []byte, count),
-		bufSize: bufSize,
-	}
-	for i := 0; i < count; i++ {
-		p.pool <- make([]byte, bufSize)
-	}
-	return p
-}
-
-func (p *bufferedPool) Get() []byte {
-	select {
-	case buf := <-p.pool:
-		return buf[:p.bufSize]
-	default:
-		return make([]byte, p.bufSize)
-	}
-}
-
-func (p *bufferedPool) Put(buf []byte) {
-	if cap(buf) < p.bufSize {
-		return
-	}
-	select {
-	case p.pool <- buf[:p.bufSize]:
-	default:
-		// pool full, discard
-	}
-}
-
-// global buffer pool instance
-var bufferPool *bufferedPool
-
-func initBufferPool() {
-	size := defaultPoolSize
-	count := defaultPoolCount
-	if config.Config_ != nil && config.Config_.Aggregator.BufferSize > 0 {
-		size = config.Config_.Aggregator.BufferSize
-	}
-	bufferPool = newBufferedPool(count, size)
-}
-
 func ListenAndServeQUIC(handler func(remoteAddr string, data []byte, l *slog.Logger), pre string, l *slog.Logger) error {
-	initBufferPool()
-
 	tlsConfig := GenerateTLSConfig()
 	addr := net.JoinHostPort("0.0.0.0", QUIC_PORT)
 
@@ -95,6 +40,7 @@ func ListenAndServeQUIC(handler func(remoteAddr string, data []byte, l *slog.Log
 }
 
 func handleConn(conn *quic.Conn, handler func(remoteAddr string, data []byte, l *slog.Logger), pre string, l *slog.Logger) {
+
 	defer conn.CloseWithError(0, "exit")
 	remote := conn.RemoteAddr().String()
 	l.Info("Connected", slog.String("remote", remote))
@@ -106,43 +52,28 @@ func handleConn(conn *quic.Conn, handler func(remoteAddr string, data []byte, l 
 			return
 		}
 
-		headerBuf := make([]byte, packet.HeaderSize)
-		_, err = io.ReadFull(stream, headerBuf)
-		if err != nil {
-			l.Error("ReadFull failed", slog.String("remote", remote), slog.Any("err", err))
-			continue
-		}
-
-		payloadLen := binary.BigEndian.Uint16(headerBuf[17:19])
-		totalLen := packet.HeaderSize + int(payloadLen)
-
-		buf := bufferPool.Get()
-		usePool := true
-
-		if cap(buf) < totalLen {
-			buf = make([]byte, totalLen)
-			usePool = false
-		} else {
-			buf = buf[:totalLen]
-		}
-
-		copy(buf, headerBuf)
-
-		_, err = io.ReadFull(stream, buf[packet.HeaderSize:])
-		if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
-			l.Error("ReadFull failed", slog.String("remote", remote), slog.Any("err", err))
-			if usePool {
-				bufferPool.Put(buf)
+		go func() {
+			headerBuf := make([]byte, packet.HeaderSize)
+			_, err := io.ReadFull(stream, headerBuf)
+			if err != nil {
+				l.Error("ReadFull failed", slog.String("remote", remote), slog.Any("err", err))
+				return
 			}
-			continue
-		}
 
-		go func(handleBuf []byte, fromPool bool) {
-			if fromPool {
-				defer bufferPool.Put(handleBuf)
+			payloadLen := binary.BigEndian.Uint16(headerBuf[17:19])
+			totalLen := packet.HeaderSize + int(payloadLen)
+
+			buf := make([]byte, totalLen)
+			copy(buf, headerBuf)
+
+			_, err = io.ReadFull(stream, buf[packet.HeaderSize:])
+			if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
+				l.Error("ReadFull failed", slog.String("remote", remote), slog.Any("err", err))
+				return
 			}
-			handler(remote, handleBuf, l)
-		}(buf, usePool)
+
+			handler(remote, buf, l)
+		}()
 	}
 }
 
