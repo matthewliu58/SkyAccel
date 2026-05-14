42274f8d2a634e32527fbbcc46353c4faaf4293d
Bootes <matthewliu58@outlook.com>
2026-05-14 15:29:20 +0800

    GenShortReqID

--- data-proxy/util/req-id.go
+++ data-proxy/util/req-id.go
@@ -1,25 +1,22 @@
 package util
 
 import (
-	"encoding/binary"
 	"net"
 	"sync/atomic"
-	"time"
 )
 
-var seq uint64
+var reqSeq uint64
 
 func GenShortReqID(clientIP string) uint32 {
 
-	no := atomic.AddUint64(&seq, 1)
-
-	now := time.Now().Unix()
-	timeByte := byte(now & 0xFF)
+	seq := atomic.AddUint64(&reqSeq, 1)
 
 	ip := net.ParseIP(clientIP).To4()
-	ip16 := binary.BigEndian.Uint16(ip[2:4])
-	
-	id := (uint32(ip16) << 16) | (uint32(timeByte) << 8) | (uint32(no) & 0xFF)
+	ipSegment := uint32(0)
+	if ip != nil {
+		ipSegment = uint32(ip[3])
+	}
 
+	id := (ipSegment << 24) | (uint32(seq) & 0xFFFFFF)
 	return id
 }
